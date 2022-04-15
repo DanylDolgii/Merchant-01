42eb26aacb366257306f5941a917ce204949772e
DanylDolgii <danyldolgii777@gmail.com>
2022-04-15 16:01:17 +0300

    Add App launcher / update class App

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
 
     static String merchantName;
@@ -14,4 +16,38 @@ public class App {
     static String infoMerchant;
     static String infoProduct;
 
+    public static void main(String[] args) {
+        initVars();
+        showData(processData());
+    }
+
+    private static void initVars() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Имя продавца: ");
+        merchantName = scanner.nextLine();
+        System.out.print("Почта: ");
+        email = scanner.nextLine();
+        System.out.print("Номер телефона: ");
+        phone = scanner.nextLine();
+        System.out.print("Наименование товара: ");
+        productName = scanner.nextLine();
+        System.out.print("Количество(шт.): ");
+        quantity = scanner.nextInt();
+        System.out.print("Цена(за 1 шт.): ");
+        price = scanner.nextDouble();
+    }
+
+    private static String processData() {
+        merchantA = new MerchantA(merchantName, phone, email);
+        infoMerchant = merchantA.infoMerchant();
+        product = new ProductA(productName, quantity, price);
+        infoProduct = product.infoProduct();
+        double sales = product.calcSales(quantity, price);
+        roundBonus = Rounder.roundValue(merchantA.calcBonus(sales));
+        return infoMerchant + infoProduct + "\nБонус (грн.): " + roundBonus;
+    }
+
+    private static void showData(String output) {
+        System.out.println(output);
+    }
 }
