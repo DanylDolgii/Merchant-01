6cd00643c23601b80b684aed4b19dda4ca20687e
DanylDolgii <danyldolgii777@gmail.com>
2022-04-15 10:59:18 +0300

    Add Product object model

--- src/app/Product.java
+++ src/app/Product.java
@@ -1,4 +1,15 @@
 package app;
 
+// Модель объекта Товара.
 public class Product {
+
+    private final String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
