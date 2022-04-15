f267fcf48d282c90e8cf54346fc45bbf839ec925
DanylDolgii <danyldolgii777@gmail.com>
2022-04-15 14:37:02 +0300

    Add variables to App class

--- src/app/App.java
+++ src/app/App.java
@@ -1,4 +1,17 @@
 package app;
 
 public class App {
+
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+    static String roundBonus;
+    static ProductA product;
+    static MerchantA merchantA;
+    static String infoMerchant;
+    static String infoProduct;
+
 }
